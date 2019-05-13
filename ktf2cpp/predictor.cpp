e68cfca72a521be413fd53fafc0195d36ffbaf16
Rory Conlin <roryconlin@gmail.com>
2019-05-13 16:10:02 -0400

    to test on sim

--- ktf2cpp/predictor.cpp
+++ ktf2cpp/predictor.cpp
@@ -5,9 +5,15 @@
 
 int main()
 {
-  fdeep::tensor5s inputs = {fdeep::tensor5(fdeep::shape5(1, 1, 1, 1, 4), {1, 2, 3, 4})};
   const auto model =  fdeep::load_model("toy_model.json");
-  fdeep::tensor5s outputs = model.predict(inputs);
+
+  fdeep::tensor5s inputs = {fdeep::tensor5(fdeep::shape5(1, 1, 1, 1, 4), {1, 2, 3, 4})};
+
+  fdeep::tensor5s outputs = {fdeep::tensor5(fdeep::shape5(1, 1, 1, 1, 3), {1, 2, 3})};
+
+  outputs = model.predict(inputs);
+  
+
   std::cout << fdeep::show_tensor5s(outputs) << std::endl; 
 
 }
