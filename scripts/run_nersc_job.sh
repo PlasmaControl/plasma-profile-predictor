57c418f6a50a7d7f6f3d5b5a6d5e66047dbdd7a1
Joe Abbate <joseph.abbate7@gmail.com>
2019-08-30 13:39:32 -0700

    Split data onto multiple nodes

--- scripts/run_nersc_job.sh
+++ scripts/run_nersc_job.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-#SBATCH -J log_1_10000
+#SBATCH -J log
 #SBATCH -C knl
-#SBATCH -N 1
+#SBATCH -N 2
 #SBATCH -q regular
-#SBATCH -t 48:00:00
-#SBATCH -o log_1_10000
+#SBATCH -t 1:00:00
+#SBATCH -o log_please_work
 #SBATCH -A m3194
 
 root_dir=$HOME/plasma-profile-predictor
 . $root_dir/scripts/setup.sh
-python $root_dir/train_NERSC_1.py
+srun -l python $root_dir/train_NERSC.py
 
--- train_NERSC.py
+++ train_NERSC.py
@@ -12,40 +12,102 @@ from models.LSTMConv1D import build_lstmconv1d_joe
 from utils.callbacks import CyclicLR, TensorBoardWrapper
 from keras.callbacks import ModelCheckpoint, ReduceLROnPlateau
 from time import strftime, localtime
+import os
 
 import horovod.keras as hvd
 from utils.device import configure_session
 
-distributed=False
+distributed=True
 
 rank, n_ranks = 0,1
 if distributed:
     hvd.init()
     rank, n_ranks = hvd.rank(), hvd.size()
+    if rank==0:
+        print('rank {}, n_ranks {}'.format(rank, n_ranks))
 
 if n_ranks  > 1:
     gpu = hvd.local_rank()
     configure_session(gpu=gpu)
 
-# config = tf.ConfigProto(intra_op_parallelism_threads=num_cores,
-#                         inter_op_parallelism_threads=num_cores,
-#                         allow_soft_placement=True,
-#                         device_count={'CPU': 1,
-#                                       'GPU': ngpu})
-# session = tf.Session(config=config)
-# K.set_session(session)
-
+profile_downsample = 2
+    
+'''
 efit_type='EFITRT1'
+input_profile_names = ['thomson_dens_{}'.format(efit_type), 'thomson_temp_{}'.format(efit_type)]
+target_profile_names = ['temp', 'dens']
+actuator_names = ['pinj', 'curr', 'tinj', 'gasA']
+profile_lookback = 1
+actuator_lookback = 10
+'''
 
-#for efit_type in ['EFITRT1','EFITRT2','EFIT01','EFIT02']:
-#for cer_type in ['cerreal','cerquick','cerauto']:
-avail_profiles = ['ffprime_{}'.format(efit_type), 'press_{}'.format(efit_type), 'ffprime_{}'.format(efit_type), 'pprime_{}'.format(efit_type),
-                  'dens', 'temp', 'idens', 'itemp', 'rotation',
-                  'thomson_dens_{}'.format(efit_type), 'thomson_temp_{}'.format(efit_type)]
-avail_actuators = ['curr', 'ech', 'gasA', 'gasB', 'gasC', 'gasD' 'gasE', 'pinj',
-                   'pinj_15L', 'pinj_15R', 'pinj_21L', 'pinj_21R', 'pinj_30L',
-                   'pinj_30R', 'pinj_33L', 'pinj_33R', 'tinj']
-available_sigs = avail_profiles + avail_actuators + ['time']
+if True:
+    processed_filename_base='/global/cscratch1/sd/abbatej/processed_data/'
+
+    with open(os.path.join(processed_filename_base,'train_{}.pkl'.format(rank)),'rb') as f:
+        traindata=pickle.load(f)
+    with open(os.path.join(processed_filename_base,'val_{}.pkl'.format(rank)),'rb') as f:
+        valdata=pickle.load(f)
+    with open(os.path.join(processed_filename_base,'param_dict.pkl'),'rb') as f:
+        param_dict=pickle.load(f)
+    globals().update(param_dict)
+    
+else:
+
+    #for efit_type in ['EFITRT1','EFITRT2','EFIT01','EFIT02']:
+    #for cer_type in ['cerreal','cerquick','cerauto']:
+    avail_profiles = ['ffprime_{}'.format(efit_type), 'press_{}'.format(efit_type), 'ffprime_{}'.format(efit_type), 'pprime_{}'.format(efit_type),
+                      'dens', 'temp', 'idens', 'itemp', 'rotation',
+                     'thomson_dens_{}'.format(efit_type), 'thomson_temp_{}'.format(efit_type)]
+    avail_actuators = ['curr', 'ech', 'gasA', 'gasB', 'gasC', 'gasD' 'gasE', 'pinj',
+                       'pinj_15L', 'pinj_15R', 'pinj_21L', 'pinj_21R', 'pinj_30L',
+                       'pinj_30R', 'pinj_33L', 'pinj_33R', 'tinj']
+    available_sigs = avail_profiles + avail_actuators + ['time']
+
+    lookbacks = {'thomson_dens_{}'.format(efit_type): profile_lookback,
+                 'thomson_temp_{}'.format(efit_type): profile_lookback,
+                 'temp': profile_lookback,
+                 'dens': profile_lookback,
+                 'rotation': profile_lookback,
+                 'press_{}'.format(efit_type): profile_lookback,
+                 'itemp': profile_lookback,
+                 'ffprime_{}'.format(efit_type): profile_lookback,
+                 'pinj': actuator_lookback,
+                 'curr': actuator_lookback,
+                 'tinj': actuator_lookback,
+                 'gasA': actuator_lookback}
+    lookahead = 8
+    # rawdata_path = '/home/fouriest/SCHOOL/Princeton/PPPL/final_data.pkl'
+    #rawdata_path = '/Users/alex/Desktop/ML/final_data_compressed.pkl'
+    rawdata_path = '/global/cscratch1/sd/abbatej/processed_shots_big/final_data_batch_{}.pkl'.format(rank)
+    flattop_only=True
+
+    # checkpt_dir = '/home/fouriest/SCHOOL/Princeton/PPPL/'
+
+    sig_names = input_profile_names + target_profile_names + actuator_names
+    normalization_method = 'StandardScaler'
+    window_length = 1
+    window_overlap = 0
+    sample_step = 3
+    uniform_normalization = True
+    train_frac = 0.8
+    val_frac = 0.2
+    nshots = 50 #TODO: replace with nbatches
+
+    assert(all(elem in available_sigs for elem in sig_names))
+
+    traindata, valdata, param_dict = process_data(rawdata_path, sig_names,
+                                              normalization_method, window_length,
+                                              window_overlap, lookbacks,
+                                              lookahead, sample_step,
+                                              uniform_normalization, train_frac,
+                                              val_frac, nshots,
+                                              flattop_only=flattop_only)
+    normalization_dict=param_dict['normalization_dict']
+
+profile_length = int(np.ceil(65/profile_downsample))
+mse_weight_vector = np.linspace(1, np.sqrt(10), profile_length)**2
+
 models = {'simple_lstm': get_model_simple_lstm,
           'lstm_conv2d': get_model_lstm_conv2d,
           'conv2d': get_model_conv2d,
@@ -53,47 +115,30 @@ models = {'simple_lstm': get_model_simple_lstm,
           'conv1d' : build_lstmconv1d_joe}
 
 model_type = 'conv1d'
-input_profile_names = ['temp', 'dens', 'rotation', 'ffprime_{}'.format(efit_type)]
-target_profile_names = ['temp', 'dens']
-actuator_names = ['pinj', 'curr', 'tinj', 'gasA']
-predict_deltas = False
-profile_lookback = 1
-actuator_lookback = 8
-lookbacks = {'temp': profile_lookback,
-             'dens': profile_lookback,
-             'rotation': profile_lookback,
-             'press_{}'.format(efit_type): profile_lookback,
-             'itemp': profile_lookback,
-             'ffprime_{}'.format(efit_type): profile_lookback,
-             'pinj': actuator_lookback,
-             'curr': actuator_lookback,
-             'tinj': actuator_lookback,
-             'gasA': actuator_lookback}
-lookahead = 4
-profile_downsample = 2
-profile_length = int(np.ceil(65/profile_downsample))
-std_activation = 'relu'
-# rawdata_path = '/home/fouriest/SCHOOL/Princeton/PPPL/final_data.pkl'
-#rawdata_path = '/Users/alex/Desktop/ML/final_data_compressed.pkl'
-rawdata_path = '/global/cscratch1/sd/abbatej/processed_shots_big/final_data_batch_0.pkl'
-flattop_only=True
 
-# checkpt_dir = '/home/fouriest/SCHOOL/Princeton/PPPL/'
+# config = tf.ConfigProto(intra_op_parallelism_cores,
+#                         inter_op_parallelism_threads=num_cores,
+#                         allow_soft_placement=True,
+#                         device_count={'CPU': 1,
+#                                       'GPU': ngpu})
+# session = tf.Session(config=config)
+# K.set_session(session)
+
+models = {'simple_lstm': get_model_simple_lstm,
+          'lstm_conv2d': get_model_lstm_conv2d,
+          'conv2d': get_model_conv2d,
+          'linear_systems': get_model_linear_systems,
+          'conv1d' : build_lstmconv1d_joe}
+predict_deltas = True
+
+std_activation = 'relu'
 checkpt_dir = "/global/homes/a/abbatej/plasma-profile-predictor/"
-sig_names = input_profile_names + target_profile_names + actuator_names
-normalization_method = 'StandardScaler'
-window_length = 1
-window_overlap = 0
-sample_step = 3
-uniform_normalization = True
-train_frac = 0.8
-val_frac = 0.2
-nshots = 100
-mse_weight_vector = np.linspace(1, np.sqrt(10), profile_length)**2
+
 hinge_weight = 50
 # batch_size = 128*ngpu
 batch_size = 128
-epochs = 100
+epochs = 5
+
 verbose = 1
 runname = 'model-' + model_type + \
           '_profiles-' + '-'.join(input_profile_names) + \
@@ -106,15 +151,6 @@ runname = 'model-' + model_type + \
           '_nshots-' + str(nshots) + \
           strftime("_%d%b%y-%H-%M", localtime())
 
-assert(all(elem in available_sigs for elem in sig_names))
-
-traindata, valdata, param_dict = process_data(rawdata_path, sig_names,
-                                              normalization_method, window_length,
-                                              window_overlap, lookbacks,
-                                              lookahead, sample_step,
-                                              uniform_normalization, train_frac,
-                                              val_frac, nshots,
-                                              flattop_only=flattop_only)
 train_generator = DataGenerator(traindata, batch_size, input_profile_names,
                                 actuator_names, target_profile_names,
                                 lookbacks, lookahead,
@@ -123,8 +159,9 @@ val_generator = DataGenerator(valdata, batch_size, input_profile_names,
                               actuator_names, target_profile_names,
                               lookbacks, lookahead,
                               predict_deltas, profile_downsample)
-steps_per_epoch = len(train_generator) // n_ranks
-val_steps = len(val_generator)
+
+steps_per_epoch = 5203 #10406 #len(train_generator) 
+val_steps = 1300 #2601 #len(val_generator)
 # model = models[model_type](input_profile_names, target_profile_names,
 #                            actuator_names, profile_lookback, actuator_lookback,
 #                            lookahead, profile_length, std_activation)
@@ -132,11 +169,9 @@ model = models[model_type](input_profile_names, target_profile_names,
                            actuator_names, profile_lookback, actuator_lookback,
                            lookahead, profile_length, std_activation)
 
-
-
 model.summary()
 
-optimizer = keras.optimizers.Adadelta()
+optimizer = keras.optimizers.Adagrad()
 if n_ranks > 1:
     optimizer = hvd.DistributedOptimizer(optimizer)
     
@@ -146,7 +181,7 @@ for sig in target_profile_names:
     loss.update({'target_'+sig: hinge_mse_loss(sig, model, hinge_weight,
                                                mse_weight_vector, predict_deltas)})
     metrics.update({'target_'+sig: []})
-    metrics['target_'+sig].append(denorm_loss(sig, model, param_dict[sig],
+    metrics['target_'+sig].append(denorm_loss(sig, model, normalization_dict[sig],
                                               keras.metrics.MAE, predict_deltas))
     metrics['target_'+sig].append(percent_correct_sign(sig, model,
                                                        predict_deltas))
@@ -176,7 +211,7 @@ model.compile(optimizer, loss, metrics)
 history = model.fit_generator(train_generator, steps_per_epoch=steps_per_epoch,
                               epochs=epochs, verbose=2, callbacks=callbacks,
                               validation_data=val_generator, validation_steps=val_steps,
-                              max_queue_size=10, workers=4, use_multiprocessing=False)
+                              max_queue_size=10, workers=4)
 
 if rank == 0:
     analysis_params = {'rawdata': rawdata_path,
@@ -199,7 +234,7 @@ if rank == 0:
                        'sample_step': sample_step,
                        'normalization_method': normalization_method,
                        'uniform_normalization': uniform_normalization,
-                       'normalization_params': param_dict,
+                       'normalization_params': normalization_dict,
                        'train_frac': train_frac,
                        'val_frac': val_frac,
                        'nshots': nshots,
@@ -214,3 +249,4 @@ if rank == 0:
 
     with open(checkpt_dir + runname + 'params.pkl', 'wb+') as f:
         pickle.dump(analysis_params, f)
+
