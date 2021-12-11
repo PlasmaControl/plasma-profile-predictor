f406d4f5aa0b8daff07a732f95eb72d408977c11
Rory Conlin <roryconlin@gmail.com>
2021-12-10 22:08:10 -0500

    Refactor slurm script generation

--- helpers/hyperparam_helpers.py
+++ helpers/hyperparam_helpers.py
@@ -1,48 +1,105 @@
 import os
-import re
 import datetime
 
 
-def make_bash_scripts(number, output_dir, ncpu, ngpu, req_mem, times, mode="traverse"):
+def make_bash_scripts(number, output_dir, ncpu, ngpu, mem, times, mode="traverse"):
     """Creates slurm scripts for batch submit jobs.
 
-    Args:
-        number (int): how many to create
-        output_dir (str): where to create the scripts
-        ncpu (int): how many CPU to request
-        nhpu (int): how many GPU to request
-        req_mem (int): how much memory to request (in GB)
-        times (list): list of estimated runtimes for jobs, in minutes)
+    Parameters
+    ----------
+    number : int
+        how many to create
+    output_dir : str
+        where to create the scripts
+    ncpu : int
+        how many CPU to request
+    nhpu : int
+        how many GPU to request
+    mem : int
+        how much memory to request (in GB)
+    times : list
+        list of estimated runtimes for jobs, in minutes
     """
 
     # make the directory
     os.makedirs(output_dir, exist_ok=True)
 
     for i in range(number):
-        with open(os.path.join(output_dir, "driver" + str(i) + ".sh"), "w+") as f:
-            f.write("#!/bin/bash \n")
-            if mode == "autoencoder":
-                f.write("#SBATCH -J LRAN_" + str(i) + " \n")
-            else:
-                f.write("#SBATCH -J CONV " + str(i) + " \n")
+        command = [
+            "root_dir=$HOME/plasma-profile-predictor",
+            "module load anaconda",
+            "conda activate tf2-gpu",
+        ]
+        if mode == "traverse":
+            command.append("python $root_dir/train_traverse.py " + str(i))
+            job = "#SBATCH -J CONV " + str(i)
+        elif mode == "autoencoder":
+            command.append("python $root_dir/train_autoencoder.py " + str(i))
+            job = "#SBATCH -J LRAN_" + str(i)
+
+        slurm_script(
+            os.path.join(output_dir, "driver" + str(i) + ".sh"),
+            command=command,
+            job_name=job,
+            ncpu=ncpu,
+            ngpu=ngpu,
+            mem=mem,
+            time=times[i],
+        )
+
+
+def slurm_script(
+    file_path,
+    command,
+    job_name=None,
+    ncpu=1,
+    ngpu=0,
+    mem=32,
+    time=60,
+    user="",
+):
+    """Create a SLURM scripts
+
+    Parameters
+    ----------
+    file_path : path-like
+        where to create file
+    command : str or list of str
+        command to run within slurm script
+    ncpu : int
+        number of CPU cores
+    cgpu : int
+        number of GPUs
+    mem : int
+        amount of memory, in GB
+    time : int
+        time, in minutes
+    user : str
+        username for mailto, assumes princeton netID. Defaults to $USER
+    """
+    with open(file_path, "w+") as f:
+        f.write("#!/bin/bash \n")
+        f.write(
+            "#SBATCH -J "
+            + str(job_name if job_name is not None else file_path.split("/")[-1])
+            + " \n"
+        )
+        if user == "":
+            user = os.environ["USER"]
+        if user is not None:
             f.write("#SBATCH --mail-type=begin \n")
             f.write("#SBATCH --mail-type=end \n")
-            f.write("#SBATCH --mail-user=wconlin@princeton.edu \n")
-            f.write("#SBATCH -N 1 \n")
-            f.write("#SBATCH -c " + str(ncpu) + "\n")
-            f.write("#SBATCH --mem " + str(req_mem) + "G\n")
-            if ngpu > 0:
-                f.write("#SBATCH -G " + str(ngpu) + "\n")
-            f.write(
-                "#SBATCH -o " + os.path.join(output_dir, "log" + str(i) + ".out \n")
-            )
-            f.write("#SBATCH -t " + str(datetime.timedelta(minutes=times[i])) + "\n")
-
-            f.write("root_dir=$HOME/plasma-profile-predictor \n")
-            f.write("module load anaconda \n")
-            f.write("conda activate tf2-gpu \n")
-            if mode == "traverse":
-                f.write("python $root_dir/train_traverse.py " + str(i) + "\n")
-            elif mode == "autoencoder":
-                f.write("python $root_dir/train_autoencoder.py " + str(i) + "\n")
-            f.write("exit")
+            f.write("#SBATCH --mail-user={}@princeton.edu \n".format(user))
+        f.write("#SBATCH -N 1 \n")
+        f.write("#SBATCH -c " + str(ncpu) + "\n")
+        f.write("#SBATCH --mem " + str(mem) + "G\n")
+        if ngpu > 0:
+            f.write("#SBATCH -G " + str(ngpu) + "\n")
+        f.write("#SBATCH -o " + file_path, +".log  \n")
+        f.write("#SBATCH -t " + str(datetime.timedelta(minutes=time)) + "\n")
+        if isinstance(command, (list, tuple)):
+            for c in command:
+                f.write(c + " \n")
+        else:
+            f.write(command + "\n")
+        f.write("exit")
